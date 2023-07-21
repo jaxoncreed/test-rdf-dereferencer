8a263f7f4e0069def550ce5bd068e3309e66c43f
jaxoncreed <jaxoncreed@gmail.com>
2023-07-21 18:50:45 -0400

    testing Ruben's thing

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import rdfDereferencer from "rdf-dereference";
+
+async function start() {
+  console.log("begin");
+  const webid = "https://pod.lightover.com/jackson2/profile/card#me";
+  try {
+    console.log((await rdfDereferencer.dereference(webid)).quads);
+  } catch (e: unknown) {
+    console.log("error");
+    console.log(e);
+  }
+  console.log("end");
+}
+start();
